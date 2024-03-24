0d554610e5b0dc333cbd7235a2589a6c284585ff
pmihaly <misi@pappmihaly.com>
2024-03-24 19:15:12 +0100

    extract recommender, add logging

---
+++ extra_item_recommender.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
+
+type ExtraItemRecommender struct {
+	driver neo4j.DriverWithContext
+	ctx    context.Context
+}
+
+func NewExtraItemRecommender(username, password, uri string, ctx context.Context) (ExtraItemRecommender, error) {
+	auth := neo4j.BasicAuth(username, password, "")
+	driver, err := neo4j.NewDriverWithContext(uri, auth)
+
+	err = driver.VerifyConnectivity(ctx)
+	if err != nil {
+		slog.Error("failed to connect to neo4j", err, "uri", uri)
+		return ExtraItemRecommender{}, err
+	}
+
+	return ExtraItemRecommender{
+		driver,
+		ctx,
+	}, nil
+}
+
+func (recommender ExtraItemRecommender) GetRecommendedExtraItems(shoppingCartId string) ([]string, error) {
+	result, err := neo4j.ExecuteQuery(recommender.ctx, recommender.driver,
+		`
+		MATCH (s1:ShoppingCart)-[:ORDERED]->(p:Product)
+		WHERE s1.shoppingCartId = $shoppingCartId
+		WITH s1, collect(DISTINCT p) AS products1
+
+		MATCH (s2:ShoppingCart)-[:ORDERED]->(p:Product)
+		WHERE s1 <> s2
+		WITH s1, s2, products1, collect(DISTINCT p) AS products2
+
+		WITH s1, s2, products1, products2, apoc.coll.intersection(products1, products2) AS intersection, apoc.coll.union(products1, products2) AS union
+
+		WITH s1, s2, intersection, union, size(intersection) * 1.0 / size(union) AS jaccard_index
+
+		ORDER BY jaccard_index DESC, s2.shoppingCartId
+		WITH s1, collect(s2)[..$neighborsCount] AS neighbors
+		WHERE size(neighbors) = $neighborsCount
+
+		UNWIND neighbors AS neighbor
+
+		MATCH (neighbor)-[:ORDERED]->(p:Product)
+		WHERE NOT (s1)-[:ORDERED]->(p)
+
+		WITH s1, p, count(DISTINCT neighbor) AS countnns
+		ORDER BY s1.shoppingCartId, countnns DESC
+
+		RETURN collect(p.productId)[..$recommendationCount] AS recommendations
+		`,
+		map[string]any{
+			"shoppingCartId":      shoppingCartId,
+			"neighborsCount":      25,
+			"recommendationCount": 5,
+		}, neo4j.EagerResultTransformer,
+		neo4j.ExecuteQueryWithDatabase("neo4j"))
+
+	if err != nil {
+		slog.Error("failed to run neo4j query", err, "shoppingCartId", shoppingCartId)
+		return make([]string, 0), err
+	}
+
+	recommendedProductIds := []string{}
+	for _, record := range result.Records {
+		recommendations, _ := record.Get("recommendations")
+		if recs, ok := recommendations.([]interface{}); ok {
+			for _, rec := range recs {
+				if str, ok := rec.(string); ok {
+					recommendedProductIds = append(recommendedProductIds, str)
+				}
+			}
+		}
+	}
+
+	return recommendedProductIds, nil
+}
+
+func (recommender ExtraItemRecommender) Close() {
+	recommender.driver.Close(recommender.ctx)
+}
--- main.go
+++ main.go
@@ -3,47 +3,61 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 
 	"mihaly.codes/shopping-cart-recommendation-engine/database"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
+	slog.SetDefault(logger)
+
 	ctx := context.Background()
 
 	conn, err := pgx.Connect(ctx, os.Getenv("POSTGRES_URL"))
 	if err != nil {
-		log.Fatalf("failed to connect to PostgreSQL: %v", err)
+		slog.Error("failed to connect to PostgreSQL", err)
+		os.Exit(1)
 	}
 	defer conn.Close(ctx)
 
 	queries := database.New(conn)
 
-	ctx1 := context.Background()
-	auth := neo4j.BasicAuth(os.Getenv("NEO4J_USER"), os.Getenv("NEO4J_PASSWORD"), "")
-	driver, err := neo4j.NewDriverWithContext(os.Getenv("NEO4J_URI"), auth)
-	defer driver.Close(ctx1)
-
-	err = driver.VerifyConnectivity(ctx1)
+	recommender, err := NewExtraItemRecommender(
+		os.Getenv("NEO4J_USER"),
+		os.Getenv("NEO4J_PASSWORD"),
+		os.Getenv("NEO4J_URI"),
+		ctx,
+	)
 	if err != nil {
-		panic(err)
+		slog.Error("failed to create recommender", err)
+		os.Exit(1)
 	}
+	defer recommender.Close()
 
 	http.HandleFunc("/products", func(w http.ResponseWriter, req *http.Request) {
+		logger.Info(
+			"incoming request",
+			"method", req.Method,
+			"path", req.URL.RequestURI(),
+			"user_agent", req.UserAgent(),
+		)
+
 		products, err := queries.ListProducts(ctx, database.ListProductsParams{Skip: 0, Take: 25})
 		if err != nil {
 			http.Error(w, "failed to fetch products", http.StatusInternalServerError)
+			slog.Error("failed to fetch products", err)
 			return
 		}
 		responseJSON, err := json.Marshal(products)
 		if err != nil {
 			http.Error(w, "failed to marshal response", http.StatusInternalServerError)
+			slog.Error("failed to marshal response", err)
 			return
 		}
 
@@ -53,16 +67,25 @@ func main() {
 	})
 
 	http.HandleFunc("/products/search", func(w http.ResponseWriter, req *http.Request) {
+		logger.Info(
+			"incoming request",
+			"method", req.Method,
+			"path", req.URL.RequestURI(),
+			"user_agent", req.UserAgent(),
+		)
+
 		searchValue := req.URL.Query().Get("q")
 
 		products, err := queries.SearchProducts(ctx, database.SearchProductsParams{SearchValue: searchValue, Skip: 0, Take: 25})
 		if err != nil {
 			http.Error(w, "failed to fetch products", http.StatusInternalServerError)
+			slog.Error("failed to fetch products", err)
 			return
 		}
 		responseJSON, err := json.Marshal(products)
 		if err != nil {
 			http.Error(w, "failed to marshal response", http.StatusInternalServerError)
+			slog.Error("failed to marshal response", err)
 			return
 		}
 
@@ -72,68 +95,31 @@ func main() {
 	})
 
 	http.HandleFunc("/products/recommended", func(w http.ResponseWriter, req *http.Request) {
+		logger.Info(
+			"incoming request",
+			"method", req.Method,
+			"path", req.URL.RequestURI(),
+			"user_agent", req.UserAgent(),
+		)
+
 		shoppingCartId := req.URL.Query().Get("shoppingCartId")
 
-		result, err := neo4j.ExecuteQuery(ctx, driver,
-			`
-			MATCH (s1:ShoppingCart)-[:ORDERED]->(p:Product)
-			WHERE s1.shoppingCartId = $shoppingCartId
-			WITH s1, collect(DISTINCT p) AS products1
-
-			MATCH (s2:ShoppingCart)-[:ORDERED]->(p:Product)
-			WHERE s1 <> s2
-			WITH s1, s2, products1, collect(DISTINCT p) AS products2
-
-			WITH s1, s2, products1, products2, apoc.coll.intersection(products1, products2) AS intersection, apoc.coll.union(products1, products2) AS union
-
-			WITH s1, s2, intersection, union, size(intersection) * 1.0 / size(union) AS jaccard_index
-
-			ORDER BY jaccard_index DESC, s2.shoppingCartId
-			WITH s1, collect(s2)[..$k] AS neighbors
-			WHERE size(neighbors) = $k
-
-			UNWIND neighbors AS neighbor
-
-			MATCH (neighbor)-[:ORDERED]->(p:Product)
-			WHERE NOT (s1)-[:ORDERED]->(p)
-
-			WITH s1, p, count(DISTINCT neighbor) AS countnns
-			ORDER BY s1.shoppingCartId, countnns DESC
-
-			RETURN collect(p.productId)[..$n] AS recommendations
-			`,
-			map[string]any{
-				"shoppingCartId": shoppingCartId,
-				"k":              25,
-				"n":              5,
-			}, neo4j.EagerResultTransformer,
-			neo4j.ExecuteQueryWithDatabase("neo4j"))
-
+		recommendedProductIds, err := recommender.GetRecommendedExtraItems(shoppingCartId)
 		if err != nil {
-			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			http.Error(w, "failed to recommend products", http.StatusInternalServerError)
 			return
-		}
-
-		recommendedProductIds := []string{}
-		for _, record := range result.Records {
-			recommendations, _ := record.Get("recommendations")
-			if recs, ok := recommendations.([]interface{}); ok {
-				for _, rec := range recs {
-					if str, ok := rec.(string); ok {
-						recommendedProductIds = append(recommendedProductIds, str)
-					}
-				}
-			}
 		}
 
 		products, err := queries.GetProductsByIds(ctx, recommendedProductIds)
 		if err != nil {
 			http.Error(w, "failed to fetch products", http.StatusInternalServerError)
+			slog.Error("failed to fetch products", err)
 			return
 		}
 		responseJSON, err := json.Marshal(products)
 		if err != nil {
 			http.Error(w, "failed to marshal response", http.StatusInternalServerError)
+			slog.Error("failed to marshal response", err)
 			return
 		}
 
