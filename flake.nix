d222dfccc69fb7f4f40000b47774f5ca0d223083
pmihaly <misi@pappmihaly.com>
2024-03-29 17:19:52 +0100

    remove lint pre commit

--- flake.nix
+++ flake.nix
@@ -2,11 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, treefmt-nix }:
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -80,17 +79,6 @@
           preBuild = "${pkgs.sqlc}/bin/sqlc generate";
         };
 
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              nixfmt.enable = true;
-              gofmt.enable = true;
-            };
-          };
-          formatting = treefmtEval.config.build.check self;
-        };
-
         devShell = nixpkgs.legacyPackages.${system}.mkShell rec {
           PGHOST = "127.0.0.1";
           PGPORT = 5432;
@@ -107,7 +95,6 @@
               builtins.toString PGPORT
             }/${PGDATABASE}";
 
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
           buildInputs = deps ++ devDeps;
         };
 
